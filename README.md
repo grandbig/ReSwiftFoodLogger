3c9bb864cba5cb92c842c0fe0d287d51f671dd2d
Takahiro Kato <takahiro.kato15@gmail.com>
2018-06-30 10:58:19 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ReSwiftSample
---
+++ ReSwiftSample/Reducer/AppReducer.swift
@@ -0,0 +1,9 @@
+//
+//  AppReducer.swift
+//  ReSwiftSample
+//
+//  Created by Takahiro Kato on 2018/06/24.
+//  Copyright © 2018年 Takahiro Kato. All rights reserved.
+//
+
+import Foundation
